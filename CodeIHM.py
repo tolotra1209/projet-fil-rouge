1660adfe3c6660ca69f0b455a0c16d6c984e80ed
GuillaumeBASTIDE <133573221+GuillaumeBASTIDE@users.noreply.github.com>
2024-01-05 16:16:43 +0100

    Update CodeIHM.py

--- CodeIHM.py
+++ CodeIHM.py
@@ -1,5 +1,6 @@
 import tkinter
 fenetre = tkinter.Tk()
+fenetre.geometry("400x250")
 
 #Creer la barre de menu
 menu_pfr = tkinter.Menu(fenetre)
@@ -10,7 +11,7 @@ menu_aides = tkinter.Menu(menu_pfr, tearoff = 0)
 menu_quitter = tkinter.Menu(menu_pfr, tearoff = 0)
 menu_pfr.add_cascade(label = "Langues", menu = menu_langues)
 menu_pfr.add_cascade(label = "Aides", menu = menu_aides)
-
+menu_pfr.add_cascade(label = "Quitter", menu = menu_quitter)
 
 #Mettre les éléments dans la cascade
 menu_aides.add_command(label = "Manuel")
@@ -21,6 +22,12 @@ menu_langues.add_command(label = "German")
 
 #Emplacement du menu
 fenetre.config(menu = menu_pfr)
+
+#Creer bouton
+quitter = tkinter.Button(fenetre, text="Cliquez ici pour quitter", command=fenetre.destroy)
+
+#Afficher bouton
+quitter.pack()
 
 #Afficher la fenetre
 fenetre.mainloop()
