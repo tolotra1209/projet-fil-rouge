02325e8cf5f6732c6b4ed41c4bbb21485bbbd540
JONATHAN <randriamarovelojonathan@gmail.com>
2024-01-08 20:31:56 +0100

    modif

--- IHM3.py
+++ IHM3.py
@@ -353,113 +353,113 @@ def recup_langue():
         fenetre.mainloop()
 
     #Espagnol
-        elif recup_langue == "Espagnol":   
-            fenetre = Tk()
-    
-            fenetre.title("Espagnol")
-            fenetre.geometry("200x450")
-    
-            #choix de la couleur
-            liste=Listbox(fenetre,height=3)
-            liste.insert(1, "Rojo")
-            liste.insert(2, "Verde")
-            liste.insert(3, "Azul")
-            liste.pack()
-    
-            label=Label(fenetre, text="Introduzca un color : ")
-            label.pack()
-            couleur_choisie=Entry(fenetre)
-            couleur_choisie.pack()
-    
-            #création d'une liste/réinitialiser la liste
-            couleur_phrase=[]
-            def get_color():
-                get_color=couleur_choisie.get()
-                if get_color == "Rojo":
-                    couleur_phrase.clear()
-                    couleur_phrase.append(get_color)
-                elif get_color == "Verde":
-                    couleur_phrase.clear()
-                    couleur_phrase.append(get_color)
-                elif get_color == "Azul":
-                    couleur_phrase.clear()
-                    couleur_phrase.append(get_color)
-                else:
-                    messagebox.showerror("ERROR", "Color no válido")
-                print(couleur_phrase)
-    
-            bouton=Button(fenetre, text="Confirme",command=get_color)
-            bouton.pack()
-    
-            
-            #choix nom
-            liste=Listbox(fenetre,height=2)
-            liste.insert(1, "Bola")
-            liste.insert(2, "Cubo")
-            liste.pack()
-    
-            label=Label(fenetre, text="Introduzca un obstáculo : ")
-            label.pack()
-            nom_choisi=Entry(fenetre)
-            nom_choisi.pack()
-    
-            #création d'une liste/réinitialiser la liste
-            nom_phrase=[]
-            def get_nom():
-                get_nom=nom_choisi.get()
-                if get_nom == "Bola":
-                    nom_phrase.clear()
-                    nom_phrase.append("la Bola")
-                elif get_nom == "Cubo":
-                    nom_phrase.clear()
-                    nom_phrase.append("el Cubo")
-                else:
-                    messagebox.showerror("ERROR", "Nombre no válido")
-    
-                print(nom_phrase)   
-    
-            bouton=Button(fenetre, text="Confirme",command=get_nom)
-            bouton.pack()
-    
-            #choix verbe
-            liste=Listbox(fenetre,height=3)
-            liste.insert(2, "Avanzar")
-            liste.insert(3, "Gire")
-            liste.pack()
-    
-            label=Label(fenetre, text="Introducir una acción : ")
-            label.pack()
-            action_choisie=Entry(fenetre)
-            action_choisie.pack()
-    
+    elif recup_langue == "Espagnol":   
+        fenetre = Tk()
+
+        fenetre.title("Espagnol")
+        fenetre.geometry("200x450")
+
+        #choix de la couleur
+        liste=Listbox(fenetre,height=3)
+        liste.insert(1, "Rojo")
+        liste.insert(2, "Verde")
+        liste.insert(3, "Azul")
+        liste.pack()
+
+        label=Label(fenetre, text="Introduzca un color : ")
+        label.pack()
+        couleur_choisie=Entry(fenetre)
+        couleur_choisie.pack()
+
         #création d'une liste/réinitialiser la liste
-            action_phrase=[]
-            def get_action():
-                get_action=action_choisie.get()
-                if get_action == "Avanzar":
-                    action_phrase.clear()
-                    action_phrase.append(get_action)
-                elif get_action == "Gire":
-                    action_phrase.clear()
-                    action_phrase.append(get_action)
-                else:
-                    messagebox.showerror("ERROR", "Acción no válida")
-    
-                print(action_phrase)   
-    
-            bouton=Button(fenetre, text="Confirme",command=get_action)
-            bouton.pack()
-    
-            label=Label(fenetre, text="Phrase")
-            label.pack()
-            def afficher_phrase():
-                label.config(text = action_phrase[0]+" hacia "+nom_phrase[0]+" "+couleur_phrase[0])
-                
-    
-            bouton=Button(fenetre, text="Valider",command=afficher_phrase)
-            bouton.pack()
-                
-            fenetre.mainloop()
+        couleur_phrase=[]
+        def get_color():
+            get_color=couleur_choisie.get()
+            if get_color == "Rojo":
+                couleur_phrase.clear()
+                couleur_phrase.append(get_color)
+            elif get_color == "Verde":
+                couleur_phrase.clear()
+                couleur_phrase.append(get_color)
+            elif get_color == "Azul":
+                couleur_phrase.clear()
+                couleur_phrase.append(get_color)
+            else:
+                messagebox.showerror("ERROR", "Color no válido")
+            print(couleur_phrase)
+
+        bouton=Button(fenetre, text="Confirme",command=get_color)
+        bouton.pack()
+
+        
+        #choix nom
+        liste=Listbox(fenetre,height=2)
+        liste.insert(1, "Bola")
+        liste.insert(2, "Cubo")
+        liste.pack()
+
+        label=Label(fenetre, text="Introduzca un obstáculo : ")
+        label.pack()
+        nom_choisi=Entry(fenetre)
+        nom_choisi.pack()
+
+        #création d'une liste/réinitialiser la liste
+        nom_phrase=[]
+        def get_nom():
+            get_nom=nom_choisi.get()
+            if get_nom == "Bola":
+                nom_phrase.clear()
+                nom_phrase.append("la Bola")
+            elif get_nom == "Cubo":
+                nom_phrase.clear()
+                nom_phrase.append("el Cubo")
+            else:
+                messagebox.showerror("ERROR", "Nombre no válido")
+
+            print(nom_phrase)   
+
+        bouton=Button(fenetre, text="Confirme",command=get_nom)
+        bouton.pack()
+
+        #choix verbe
+        liste=Listbox(fenetre,height=3)
+        liste.insert(2, "Avanzar")
+        liste.insert(3, "Gire")
+        liste.pack()
+
+        label=Label(fenetre, text="Introducir una acción : ")
+        label.pack()
+        action_choisie=Entry(fenetre)
+        action_choisie.pack()
+
+    #création d'une liste/réinitialiser la liste
+        action_phrase=[]
+        def get_action():
+            get_action=action_choisie.get()
+            if get_action == "Avanzar":
+                action_phrase.clear()
+                action_phrase.append(get_action)
+            elif get_action == "Gire":
+                action_phrase.clear()
+                action_phrase.append(get_action)
+            else:
+                messagebox.showerror("ERROR", "Acción no válida")
+
+            print(action_phrase)   
+
+        bouton=Button(fenetre, text="Confirme",command=get_action)
+        bouton.pack()
+
+        label=Label(fenetre, text="Phrase")
+        label.pack()
+        def afficher_phrase():
+            label.config(text = action_phrase[0]+" hacia "+nom_phrase[0]+" "+couleur_phrase[0])
+            
+
+        bouton=Button(fenetre, text="Valider",command=afficher_phrase)
+        bouton.pack()
+            
+        fenetre.mainloop()
 
     #ERROR
     else:
