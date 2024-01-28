de2fbc58a13181a043ac5f1a589a133aab180793
tolotra1209 <150174630+tolotra1209@users.noreply.github.com>
2024-01-28 10:10:29 +0100

    Update main_Final_Pour_De_Vrai.c

--- main_Final_Pour_De_Vrai.c
+++ main_Final_Pour_De_Vrai.c
@@ -7,6 +7,8 @@
 #include "choix_langue.h"
 #include "requete_texte.h"
 #include "conversion_requete.h"
+#include "execution.h"
+
 
 int main() {
 
@@ -123,8 +125,8 @@ int main() {
 
     printf("\n");
 
-    /*liste chainéé pour synonyme*/
-    SynonymeCell* head5 = NULL; // Initialisation de la liste chaînée
+    /*liste chainéé pour synonymeAvance*/
+    SynonymeAvanceCell* head5 = NULL; // Initialisation de la liste chaînée
 
     // Ouvrir le fichier texte.txt en mode lecture
     FILE* fichier5 = fopen("synonyme_avance.txt", "r");
@@ -139,15 +141,44 @@ int main() {
     while (fgets(buffer5, sizeof(buffer5), fichier5) != NULL) {
         // Supprimer le saut de ligne à la fin de la chaîne
         buffer5[strcspn(buffer5, "\n")] = '\0';
-        appendSynonyme(&head5, buffer5);
+        appendSynonymeAvance(&head5, buffer5);
     }
 
     // Fermer le fichier après la lecture
     fclose(fichier5);
 
     // Afficher le contenu de la liste chaînée
-    printf("Contenu de la liste chainee de synonyme :\n");
-    displaySynonyme(head5);
+    printf("Contenu de la liste chainee de synonyme d'avance :\n");
+    displaySynonymeAvance(head5);
+    
+    printf("\n");
+    
+    /*liste chainéé pour synonymeTourne*/
+    SynonymeTourneCell* head6 = NULL; // Initialisation de la liste chaînée
+
+    // Ouvrir le fichier texte.txt en mode lecture
+    FILE* fichier6 = fopen("synonyme_tourne.txt", "r");
+    if (fichier6 == NULL) {
+        perror("Erreur lors de l'ouverture du fichier");
+        return EXIT_FAILURE;
+    }
+
+    char buffer6[MAX];
+
+    // Lecture du fichier ligne par ligne et ajout dans la liste chaînée
+    while (fgets(buffer6, sizeof(buffer6), fichier6) != NULL) {
+        // Supprimer le saut de ligne à la fin de la chaîne
+        buffer6[strcspn(buffer6, "\n")] = '\0';
+        appendSynonymeTourne(&head6, buffer6);
+    }
+
+    // Fermer le fichier après la lecture
+    fclose(fichier6);
+
+    // Afficher le contenu de la liste chaînée
+    printf("Contenu de la liste chainee de synonyme de Tourne :\n");
+    displaySynonymeTourne(head6);
+    
     printf("\n");
     
     choisir_langue();
@@ -171,53 +202,59 @@ int main() {
     printf("Combien de requêtes voulez-vous entrer ? ");
     scanf("%d", &nbRequetes);
     getchar();  // Absorber le caractère de nouvelle ligne laissé par scanf
+    effacer_contenu_fichier("commande.txt");
 
     for (int i = 0; i < nbRequetes; i++) {
         char requete[MAX];
         saisir_texte(requete, "requete_valide.txt");
 
         analyser_phrases(verbes, nbVerbes, noms, nbNoms, couleurs, nbCouleurs, requete);
+        
+
+		FILE *fichierRequete = fopen("requete_valide.txt", "r");
+		FILE *fichierCommande;
+
+		if (fichierRequete == NULL) {
+			perror("Erreur lors de l'ouverture du fichier de requête");
+			exit(EXIT_FAILURE);
+		}
+
+		char mot[100]; // Taille maximale d'un mot, ajustez si nécessaire
+
+		while (fscanf(fichierRequete, "%s", mot) == 1) {
+			// Ouvrir le fichier de commande en mode "a" pour ajouter du contenu sans effacer
+			fichierCommande = fopen("commande.txt", "a");
+			if (fichierCommande == NULL) {
+				perror("Erreur lors de l'ouverture du fichier de commande");
+				exit(EXIT_FAILURE);
+			}
+
+			if (estPresentDansFichier(mot, "synonyme_avance.txt")) {
+				fprintf(fichierCommande, "Go_forward()");
+			} else if (estPresentDansFichier(mot, "synonyme_tourne.txt")) {
+				fprintf(fichierCommande, "Turn()");
+			}
+
+			fprintf(fichierCommande, "\n"); // Ajouter toujours un saut de ligne
+
+			fclose(fichierCommande);
+			
+			printf("\n");
+		}
+
+		fclose(fichierRequete);
+
     }
+    
+    printf("\n");
+    
+    executer_commande();
 
     printf("\n");
 
      // Effacer le contenu du fichier "commande.txt" au début de chaque exécution
-    effacer_contenu_fichier("commande.txt");
-
-    FILE *fichierRequete = fopen("requete_valide.txt", "r");
-    FILE *fichierCommande;
-
-    if (fichierRequete == NULL) {
-        perror("Erreur lors de l'ouverture du fichier de requête");
-        exit(EXIT_FAILURE);
-    }
-
-    char mot[100]; // Taille maximale d'un mot, ajustez si nécessaire
-
-    while (fscanf(fichierRequete, "%s", mot) == 1) {
-        // Ouvrir le fichier de commande en mode "a" pour ajouter du contenu sans effacer
-        fichierCommande = fopen("commande.txt", "a");
-        if (fichierCommande == NULL) {
-            perror("Erreur lors de l'ouverture du fichier de commande");
-            exit(EXIT_FAILURE);
-        }
-
-        if (estPresentDansFichier(mot, "synonyme_avance.txt")) {
-            fprintf(fichierCommande, "Go_forward()");
-        } else if (estPresentDansFichier(mot, "synonyme_tourne.txt")) {
-            fprintf(fichierCommande, "Turn()");
-        }
-
-        fprintf(fichierCommande, "\n"); // Ajouter toujours un saut de ligne
-
-        fclose(fichierCommande);
-    }
-
-    fclose(fichierRequete);
-
-    printf("Les commandes ont été enregistrées.\n");
-
-
-
+    
+    
     return 0;
 }
+
